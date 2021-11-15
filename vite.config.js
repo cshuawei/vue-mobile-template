1d0fe56fd8e811e181df6039b19f0a0606e69f8a
cshuawei <cshuawei@vip.qq.com>
2021-11-15 15:18:10 +0800

    initial commint

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@/',
+        replacement: '/src/'
+      }
+    ]
+  },
+  server: {
+    host: '0.0.0.0'
+  },
+  build: {
+    assetsDir: '.'
+  }
+})
